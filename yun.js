75905cdd706d5f0d9453ce6795e88d4b97243162
XiYinadi <1905300166@qq.com>
2025-05-23 23:57:44 +0800

    Update yun.js

--- yun.js
+++ yun.js
@@ -6,7 +6,7 @@ async function  add() {
     const timestamp = `${now.getFullYear()}-${String(now.getMonth()+1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')} ${String(now.getHours()).padStart(2, '0')}:${String(now.getMinutes()).padStart(2, '0')}:${String(now.getSeconds()).padStart(2, '0')}`;
     
     await axios({
-        url:'https://02-hazel.vercel.app/',
+        url:'https://02-hazel.vercel.app/api/submit',
         method: 'POST',
         data: {
             title: input,
